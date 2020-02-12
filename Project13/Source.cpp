94aae7b930afe9adec6230425554b703be92c8c6
olgakovalyova2000 <kovaleva.od@phystech.edu>
2020-02-12 22:04:54 +0300

    delete all

--- Project13/Source.cpp
+++ Project13/Source.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
-#include <string>
+
+
 int main()
 {
-	std::cout << "Hello, world!" << std::endl;
-	std::cout << "I'm glad to see you." << std::endl;
-	std::cout << "Because I didn't see you so long." << std::endl;
-	std::cout << "Night." << std::endl;
-	std::cout << "Day." << std::endl;
-	std::cout << "That's all for you." << std::endl;
-	std::cout << "All for me." << std::endl;
-	std::cout << ":)" << std::endl;
-	std::cout << ":))" << std::endl;
+	
 }
