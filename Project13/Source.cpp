000abe51167cba464d74d95d23d752fc35b38802
olgakovalyova2000 <kovaleva.od@phystech.edu>
2020-02-12 20:49:29 +0300

    typing

--- Project13/Source.cpp
+++ Project13/Source.cpp
@@ -4,4 +4,7 @@ int main()
 {
 	std::cout << "Hello, world!" << std::endl;
 	std::cout << "I'm glad to see you." << std::endl;
+	std::cout << "Because I didn't see you so long." << std::endl;
+	std::cout << "Night." << std::endl;
+	std::cout << "Day." << std::endl;
 }
